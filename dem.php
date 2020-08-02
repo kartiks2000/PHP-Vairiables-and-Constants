bbc1c60cdd5774f4a5636648c6fec81d67c3440d
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-02 17:22:22 +0530

    Add files via upload

---
+++ dem.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Vairiables and constants in PHP</h1>
+    <?php    
+
+        // Vairiables
+
+        $x = 20;
+        echo $x . "<br>";
+
+        $x = 100;
+        echo $x . "<br>";
+
+
+        // Constant
+        
+        define("y",80);
+        echo y . "<br>";
+    ?>
+</body>
+</html>
